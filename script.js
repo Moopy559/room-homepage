60f324f6fc8c11a8e300fb421609e8f8f5db7b80
Moopy559 <125260487+Moopy559@users.noreply.github.com>
2024-02-11 13:34:04 +1000

    Add js for responsiveness - JS identifies screen size and returns either desktop or mobile - returned value determines which hero image is loaded, desktop version or the mobile version

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+const screenWidth = window.innerWidth;
+let screen = screenWidth < 400 ? "mobile" : "desktop";
 const leftArrow = document.getElementById("left-arrow");
 const rightArrow = document.getElementById("right-arrow");
 const heroImage = document.getElementById("hero-image");
 const header = document.getElementById("header");
 const blurb = document.getElementById("blurb");
 let count = 1;
+updateContent();
 
 leftArrow.addEventListener("click", () => {
   decrementCount();
@@ -26,19 +29,19 @@ function incrementCount() {
 function updateContent() {
   switch (count) {
     case 1:
-      heroImage.setAttribute("src", "./images/desktop-image-hero-1.jpg");
+      heroImage.setAttribute("src", `./images/${screen}-image-hero-1.jpg`);
       header.textContent = "Discover innovative ways to decorate";
       blurb.textContent =
         "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.";
       break;
     case 2:
-      heroImage.setAttribute("src", "./images/desktop-image-hero-2.jpg");
+      heroImage.setAttribute("src", `./images/${screen}-image-hero-2.jpg`);
       header.textContent = "We are available all across the globe";
       blurb.textContent =
         "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.";
       break;
     case 3:
-      heroImage.setAttribute("src", "./images/desktop-image-hero-3.jpg");
+      heroImage.setAttribute("src", `./images/${screen}-image-hero-3.jpg`);
       header.textContent = "Manufactured with the best materials";
       blurb.textContent =
         "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.";
